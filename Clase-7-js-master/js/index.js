2e26f77661c6ccce1eb825dd9f727a583e782607
emmonina <54905708+emmonina@users.noreply.github.com>
2019-10-16 15:55:24 -0300

    Add files via upload

---
+++ Clase-7-js-master/js/index.js
@@ -0,0 +1,46 @@
+window.addEventListener ('load', function () {
+    let divTabla = document.getElementById ('divTabla');
+    let tabla = crearTabla (personas);
+    divTabla.appendChild (tabla);
+});
+
+
+
+function crearTabla(arr) {
+    
+let tabla = document.createElement ('table');
+
+let encabezado =document.createElement('tr');
+
+for (prop in arr[0]) { 
+let th = document.createElement ('th');
+th.textContent = prop;
+th.setAttribute ('background', 'red');
+
+encabezado.appendChild (th);
+
+}
+
+tabla.appendChild (encabezado); 
+
+for ( let i=0;  i < arr.length; i++)
+{
+let fila = document.createElement ('tr');
+
+for (prop in arr [i]) {
+let celda = document.createElement ('td');
+celda.textContent =  arr [i] [prop];
+fila.appendChild(celda);
+
+
+}
+    
+tabla.appendChild(fila);
+
+}
+
+
+
+
+return tabla;
+}
